c40e2a09f22db8e458b6c6cc827c490097b63b31
MinJun Choi <mj043000@naver.com>
2022-11-12 02:05:04 +0900

    Refac: refactory CSphere, CWall, CLight

---
+++ CWall.h
@@ -0,0 +1,34 @@
+#ifndef _CWALL_
+#define _CWALL_
+
+#include "d3dUtility.h"
+#include "CSphere.h"
+
+class CWall {
+private:
+    float m_x;
+    float m_z;
+    float m_width;
+    float m_depth;
+    float m_height;
+
+    D3DXMATRIX              m_mLocal;
+    D3DMATERIAL9            m_mtrl;
+    ID3DXMesh* m_pBoundMesh;
+
+    void setLocalTransform(const D3DXMATRIX& mLocal);
+
+public:
+    CWall(void);
+    ~CWall(void);
+    bool create(IDirect3DDevice9* pDevice, float ix, float iz, float iwidth, float iheight, float idepth, D3DXCOLOR color);
+    void destroy(void);
+    void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld);
+    bool hasIntersected(CSphere& ball);
+    void hitBy(CSphere& ball);
+    void setPosition(float x, float y, float z);
+    float getHeight(void) const;
+ 
+};
+
+#endif
